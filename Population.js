e2b2dc5103d0f7dd692dad5cbd212e087f8fdad7
AnthonyOlijnyk <anthony.olijnykabo@gmail.com>
2019-05-01 23:08:50 -0400

    Made it about bubbles

--- Population.js
+++ Population.js
@@ -1,55 +1,55 @@
 
 function Population(){
-  this.rockets = [];
+  this.bubbles = [];
   this.popsize = 100;
   this.matingpool = [];
 
   for(var i = 0; i < this.popsize; i++){
-    this.rockets[i] = new Rocket();
+    this.bubbles[i] = new Bubble();
   }
 
   this.evaluate = function(){
 
     var maxfit = 0;
     for(var i = 0; i < this.popsize; i++){
-      this.rockets[i].calcFitness();
-      if(this.rockets[i].fitness > maxfit){
-        maxfit = this.rockets[i].fitness;
+      this.bubbles[i].calcFitness();
+      if(this.bubbles[i].fitness > maxfit){
+        maxfit = this.bubbles[i].fitness;
       }
     }
 
 
     for(var i = 0; i < this.popsize; i++){
-      this.rockets[i].fitness /= maxfit; // makes sure everything is between 0 and 1
+      this.bubbles[i].fitness /= maxfit; // makes sure everything is between 0 and 1
     }
 
     this.matingpool = [];
     //adds the number of participants to the matingpool based off
     //of their fitness value
     for(var i = 0; i < this.popsize; i++){
-      var n = this.rockets[i].fitness * 100;//recall fitness between 0 and 1, this line gets the fitness value and multiplies it by 100
+      var n = this.bubbles[i].fitness * 100;//recall fitness between 0 and 1, this line gets the fitness value and multiplies it by 100
       for(var j = 0; j < n; j++){
-        this.matingpool.push(this.rockets[i]);// adds a certain number of participants to the matingpool and this number is determined from the multiplication of the fitness times 100
+        this.matingpool.push(this.bubbles[i]);// adds a certain number of participants to the matingpool and this number is determined from the multiplication of the fitness times 100
       }
     }
   }
 
   this.selection = function(){
-    var newRockets = [];
-    for(var i = 0; i < this.rockets.length; i++){
+    var newBubbles = [];
+    for(var i = 0; i < this.bubbles.length; i++){
       var parentA = random(this.matingpool).dna;
       var parentB = random(this.matingpool).dna;
       var child = parentA.crossover(parentB);
       child.mutation();
-      newRockets[i] = new Rocket(child);
+      newBubbles[i] = new Bubble(child);
     }
-    this.rockets = newRockets;
+    this.bubbles = newBubbles;
   }
 
   this.run = function(){
     for(var i = 0; i < this.popsize; i++){
-      this.rockets[i].update();
-      this.rockets[i].show();
+      this.bubbles[i].update();
+      this.bubbles[i].show();
     }
   }
 }
